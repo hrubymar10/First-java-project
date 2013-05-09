b215127143cd28434c0c65891e9be7b83da46ee9
Sofartin <hrubymar10@gmail.com>
2013-05-09 19:32:47 +0200

    added: System.out.println("Hello devbook!");

--- src/first/java/project/FirstJavaProject.java
+++ src/first/java/project/FirstJavaProject.java
@@ -14,6 +14,6 @@ public class FirstJavaProject {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello devbook!");
     }
 }
