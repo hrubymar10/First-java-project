814ff514885699516caa7f604e9e27fd791e3eaa
Sofartin <hrubymar10@gmail.com>
2013-05-09 20:12:22 +0200

    Created project double up

---
+++ double up/src/pkgdouble/up/DoubleUp.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkgdouble.up;
+
+/**
+ *
+ * @author martin
+ */
+public class DoubleUp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Zadejte číslo k zdvojnásobení:");
+int a = sc.nextLine();
+a = a * 2;
+System.out.println(a);
+    }
+}
