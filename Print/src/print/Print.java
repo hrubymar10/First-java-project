7a8f4ffc12693568f833d46b5a1637bb03056ec6
Sofartin <hrubymar10@gmail.com>
2013-05-09 19:49:51 +0200

    Created new project Print

---
+++ Print/src/print/Print.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package print;
+
+/**
+ *
+ * @author martin
+ */
+public class Print {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a;
+a = 56;
+System.out.println(a);
+    }
+}
