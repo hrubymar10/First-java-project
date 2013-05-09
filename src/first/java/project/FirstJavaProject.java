c146fba6ea35418dfbe46be57962b3fb4874f6e8
Sofartin <hrubymar10@gmail.com>
2013-05-09 18:37:28 +0200

    Start
    New project created in NetBeans IDE 7.3

---
+++ src/first/java/project/FirstJavaProject.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package first.java.project;
+
+/**
+ *
+ * @author martin
+ */
+public class FirstJavaProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
